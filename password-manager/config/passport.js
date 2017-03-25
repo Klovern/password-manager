d34518480f0296b1f19b474b45b3548e13473fa7
Klovern <oskar.kindeland@gmail.com>
2017-03-25 21:44:28 +0100

    Added authentication inside local login for ip

--- password-manager/config/passport.js
+++ password-manager/config/passport.js
@@ -25,15 +25,12 @@ passport.use('local-login', new LocalStrategy({
 
      // find a user whose email is the same as the forms email
      // we are checking to see if the user trying to login already exists
-     console.log(`Ip trying to connect ${req.ip}`);
      User.findOne({ 'local.email' :  email }, function(err, user) {
          // if there are any errors, return the error before anything else
          if (err)
              return done(err);
          // if no user is found, return the message
 
-         if(!user.local.whIp == req.ip)
-            return done(null, false, req.flash('loginMessage', 'Your ip is not authorized.'));
          if (!user)
              return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
 
@@ -41,25 +38,16 @@ passport.use('local-login', new LocalStrategy({
          if (!user.validPassword(password))
              return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
 
+             if(user.local.whIp != 'undefined'){
+               console.log(user.local.whIp + "\t" +req.ip);
+                 if(user.local.whIp != req.ip)
+                    return done(null, false, req.flash('loginMessage', 'Your ip is not authorized.'));
+                   }
          // all is well, return successful user
          return done(null, user);
      });
 
  }));
-
-passport.use(new LocalStrategy(function(req, username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
-  }
-));
 
 passport.use('local-signup', new LocalStrategy({
      // by default, local strategy uses username and password, we will override with email
